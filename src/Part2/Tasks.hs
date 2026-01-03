97326113beb6171a8e17b0be0e77b4b6585c108e
Jegor Popow <90181671+jegorpopow@users.noreply.github.com>
2026-01-03 13:51:09 +0300

    Update Tasks.hs

--- src/Part2/Tasks.hs
+++ src/Part2/Tasks.hs
@@ -11,19 +11,40 @@ data Term = IntConstant { intValue :: Int }          -- числовая кон�
 
 -- Для бинарных операций необходима не только реализация, но и адекватные
 -- ассоциативность и приоритет
+
+infixl 6 |+|
+infixl 6 |-|
+infixl 7 |*|
+
 (|+|) :: Term -> Term -> Term
-(|+|) = notImplementedYet
+(|+|) = BinaryTerm Plus
 (|-|) :: Term -> Term -> Term
-(|-|) = notImplementedYet
+(|-|) = BinaryTerm Minus
 (|*|) :: Term -> Term -> Term
-(|*|) = notImplementedYet
+(|*|) = BinaryTerm Times
 
 -- Заменить переменную `varName` на `replacement`
 -- во всём выражении `expression`
 replaceVar :: String -> Term -> Term -> Term
-replaceVar varName replacement expression = notImplementedYet
+replaceVar varName replacement i@IntConstant{intValue=intValue} = i
+replaceVar varName replacement v@Variable{varName=varName'} 
+  | varName == varName' = replacement
+  | otherwise = v
+replaceVar varName replacement BinaryTerm{op=op, lhv=lhv, rhv=rhv} = BinaryTerm op lhs' rhs'
+  where 
+    lhs' = (replaceVar varName replacement lhv)
+    rhs' = (replaceVar varName replacement rhv)
 
 -- Посчитать значение выражения `Term`
 -- если оно состоит только из констант
 evaluate :: Term -> Term
-evaluate = notImplementedYet
+evaluate i@IntConstant{intValue=_} = i
+evaluate v@Variable{varName=_} = v
+evaluate b@BinaryTerm {op=op, lhv=lhv, rhv=rhv} = 
+   case (evaluate lhv, evaluate rhv) of 
+      (IntConstant{intValue=lhv'}, IntConstant{intValue=rhv'}) -> IntConstant $ apply op lhv' rhv'
+      (_ , _) -> b
+   where
+    apply Plus = (+)
+    apply Minus = (-)
+    apply Times = (*) 
