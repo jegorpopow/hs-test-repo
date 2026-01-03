12c2ceccf1dd44a1f182d2bc53422f5fc1d13b67
Jegor Popow <90181671+jegorpopow@users.noreply.github.com>
2026-01-03 13:52:24 +0300

    Update Tasks.hs

--- src/Part5/Tasks.hs
+++ src/Part5/Tasks.hs
@@ -4,29 +4,31 @@ import Util(notImplementedYet)
 
 -- Реализуйте левую свёртку
 myFoldl :: (b -> a -> b) -> b -> [a] -> b
-myFoldl = notImplementedYet
+myFoldl _ i [] = i 
+myFoldl f i (h : t) = myFoldl f (f i h) t 
 
 -- Реализуйте правую свёртку
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr = notImplementedYet
+myFoldr _ i [] = i
+myFoldr f i (h : t) = f h $ myFoldr f i t
 
 -- Используя реализации свёрток выше, реализуйте все остальные функции в данном файле
 
 myMap :: (a -> b) -> [a] -> [b]
-myMap f = notImplementedYet
+myMap f = myFoldr (\ a acc -> f a : acc) [] 
 
 myConcatMap :: (a -> [b]) -> [a] -> [b]
-myConcatMap f = notImplementedYet
+myConcatMap f = foldr (\ a acc -> foldr (:) acc $ f a) [] 
 
 myConcat :: [[a]] -> [a]
-myConcat = notImplementedYet
+myConcat = myConcatMap id 
 
 myReverse :: [a] -> [a]
-myReverse = notImplementedYet
+myReverse = myFoldl (\ acc a -> a : acc) []
 
 myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter p = notImplementedYet
+myFilter p = myFoldr (\ a acc -> if p a then a : acc else acc) [] 
 
 myPartition :: (a -> Bool) -> [a] -> ([a], [a])
-myPartition p = notImplementedYet
+myPartition p = myFoldr (\ a (l, r) -> if p a then (a : l, r) else (l, a : r))  ([], [])  
 
